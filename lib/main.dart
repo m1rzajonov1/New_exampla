6537b1b5c091d4bbc2d6748185222e7f38259c36
m1rzajonov1 <m1rzajonov1@mail.ru>
2022-05-17 01:03:34 +0500

    When I logged into the program, I linked the Loader page to the Home page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+      ),initialRoute: '/loader',
       onGenerateRoute: Routes.instance.onGenerateRoue,
     );
   }
--- lib/screens/loader_page.dart
+++ lib/screens/loader_page.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:newproectap/core/extensions/mediaquary_extension.dart';
 
-class LoaderPage extends StatelessWidget {
+class LoaderPage extends StatefulWidget {
   const LoaderPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoaderPage> createState() => _LoaderPageState();
+}
+
+class _LoaderPageState extends State<LoaderPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,19 +25,18 @@ class LoaderPage extends StatelessWidget {
       body: Column(
         children: [
           Padding(
-            padding:  EdgeInsets.only(top: context.h*0.4),
+            padding: EdgeInsets.only(top: context.h * 0.4),
             child: Center(
                 child: SizedBox(
-              height: context.h*0.2,
-              width: context.w*0.5,
+              height: context.h * 0.2,
+              width: context.w * 0.5,
               child: SvgPicture.asset("assets/svg/Group 734App.svg"),
             )),
           ),
-           Padding(
-             padding:  EdgeInsets.only(top: context.w*0.6),
-             child: SvgPicture.asset("assets/svg/Group 733title.svg"),
-           ),
-          
+          Padding(
+            padding: EdgeInsets.only(top: context.w * 0.6),
+            child: SvgPicture.asset("assets/svg/Group 733title.svg"),
+          ),
         ],
       ),
     );
